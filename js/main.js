d054c884b986273bd58dbab5efe1fb79fb3bc76f
Samir Cəbrayıllı <samirjabrailli98@gmail.com>
2025-10-20 10:48:38 +0400

    js le animasiya elave edildi

--- js/main.js
+++ js/main.js
@@ -42,3 +42,40 @@
             pagesDropdown.classList.add('hidden');
         }
     });
+
+// animation
+   // GSAP və ScrollTrigger aktivləşdiririk
+gsap.registerPlugin(ScrollTrigger);
+
+// Card section üçün animasiya
+gsap.from(".card", {
+  scrollTrigger: {
+    trigger: ".card",      // nə vaxt başlayacaq
+    start: "top 80%",      // səhifənin 80%-də görünəndə
+    toggleActions: "play none none none", // bir dəfə oynasın
+  },
+  duration: 1.2,           // animasiyanın müddəti
+  y: 100,                  // aşağıdan yuxarı
+  x: -100,                 // soldan sağa
+  opacity: 0,              // başlanğıc şəffaflıq
+  ease: "power3.out"       // smooth keçid
+});
+
+// Hər img üçün ayrıca animasiya (aşağıdan-yuxarı və tərsinə)
+gsap.registerPlugin(ScrollTrigger);
+
+// Şəkillər üçün animasiya
+gsap.utils.toArray(".card img").forEach((img) => {
+  gsap.from(img, {
+    y: 100,          // aşağıdan yuxarı
+    x: -100,         // soldan sağa
+    opacity: 0,      // şəffaflıqdan görünür
+    duration: 1.2,   // animasiyanın müddəti
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: img,
+      start: "top 90%",       // şəkil ekrana yaxınlaşanda başlasın
+      toggleActions: "play none none none", // bir dəfə oynasın
+    },
+  });
+});
